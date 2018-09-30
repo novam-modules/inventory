73aca5d5e803a9010bc13a8d5af6abba9b09fcc6
Hilkiah Makemo <hilmak01@gmail.com>
2018-09-30 05:13:58 -0600

    Original from local v4

---
+++ Resources/views/layouts/master.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.admin')
+@section('head')
+    <title>Module Inventory {{ isset($title)? " :: $title": '' }}</title>
+@endsection
+@section('styles')
+{{-- <link rel="stylesheet" href="{{ mix('css/inventory.css') }}"> --}}
+@endsection
+@section('layout')
+<div id="inventory" class="inventory">
+    @yield('content')
+</div>
+@endsection
+@section('scripts')
+{{-- <script src="{{ mix('js/inventory.js') }}"></script> --}}
+@endsection
