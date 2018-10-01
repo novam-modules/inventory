18daee4660ccdbda06d7366c946ae98c91dbea4f
Hilkiah Makemo <hilmak01@gmail.com>
2018-10-01 06:45:50 -0600

    Updated index view

--- Resources/views/index.blade.php
+++ Resources/views/index.blade.php
@@ -1,9 +1,42 @@
 @extends('inventory::layouts.master')
 
 @section('content')
-    <h1>Hello World</h1>
+    <h1>Inventory</h1>
 
-    <p>
-        This view is loaded from module: {!! config('inventory.name') !!}
-    </p>
+    <div class="card-deck mb-3">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title m-0">Fleet</h4>
+            </div>
+            <div class="card-body">
+                <p class="card-text">Text</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title m-0">Materials</h4>
+            </div>
+            <div class="card-body">
+                <p class="card-text">Text</p>
+            </div>
+        </div>
+    </div>
+    <div class="card-deck my-3">
+        <div class="card">
+            <div class="card-header">
+                <h4 class="card-title m-0">Equipment</h4>
+            </div>
+            <div class="card-body">
+                <p class="card-text">Text</p>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-header bg-success">
+                <h4 class="card-title m-0">Ledger</h4>
+            </div>
+            <div class="card-body">
+                <p class="card-text">Text</p>
+            </div>
+        </div>
+    </div>
 @stop
