f025e57dc35203ca7cbf44eee3e694211889b6de
Hilkiah Makemo <hilmak01@gmail.com>
2018-10-30 19:42:09 -0600

    Modified Admin to Modules

--- Providers/InventoryServiceProvider.php
+++ Providers/InventoryServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Admin\Inventory\Providers;
+namespace Modules\Inventory\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Database\Eloquent\Factory;
@@ -91,7 +91,7 @@ class InventoryServiceProvider extends ServiceProvider
 
     /**
      * Register an additional directory of factories.
-     * 
+     *
      * @return void
      */
     public function registerFactories()
--- Providers/RouteServiceProvider.php
+++ Providers/RouteServiceProvider.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Admin\Inventory\Providers;
+namespace Modules\Inventory\Providers;
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
@@ -12,7 +12,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    protected $namespace = 'Admin\Inventory\Http\Controllers';
+    protected $namespace = 'Modules\Inventory\Http\Controllers';
 
     /**
      * Called before routes are registered.
